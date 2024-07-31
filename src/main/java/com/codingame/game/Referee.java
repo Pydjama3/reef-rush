4d0fb764cf6a9d55092813d6e94d39e388a265a5
Elias TOURNIER CORRIGNAN <elias.tournier@gmail.com>
2024-07-31 23:26:07 +0200

    Initial commit

---
+++ src/main/java/com/codingame/game/Referee.java
@@ -0,0 +1,37 @@
+package com.codingame.game;
+import java.util.List;
+
+import com.codingame.gameengine.core.AbstractPlayer.TimeoutException;
+import com.codingame.gameengine.core.AbstractReferee;
+import com.codingame.gameengine.core.MultiplayerGameManager;
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.google.inject.Inject;
+
+public class Referee extends AbstractReferee {
+    // Uncomment the line below and comment the line under it to create a Solo Game
+    // @Inject private SoloGameManager<Player> gameManager;
+    @Inject private MultiplayerGameManager<Player> gameManager;
+    @Inject private GraphicEntityModule graphicEntityModule;
+
+    @Override
+    public void init() {
+        // Initialize your game here.
+    }
+
+    @Override
+    public void gameTurn(int turn) {
+        for (Player player : gameManager.getActivePlayers()) {
+            player.sendInputLine("input");
+            player.execute();
+        }
+
+        for (Player player : gameManager.getActivePlayers()) {
+            try {
+                List<String> outputs = player.getOutputs();
+                // Check validity of the player output and compute the new game state
+            } catch (TimeoutException e) {
+                player.deactivate(String.format("$%d timeout!", player.getIndex()));
+            }
+        }        
+    }
+}
