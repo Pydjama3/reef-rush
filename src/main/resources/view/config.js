b906100a766fd0d1135325a2ae2328b82d115e28
Elias TOURNIER CORRIGNAN <elias.tournier@gmail.com>
2024-07-31 23:30:19 +0200

    Revert "Initial commit"
    This reverts commit 4d0fb764cf6a9d55092813d6e94d39e388a265a5.

--- src/main/resources/view/config.js
+++
@@ -1,6 +0,0 @@
-import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
-
-// List of viewer modules that you want to use in your game
-export const modules = [
-	GraphicEntityModule
-];
